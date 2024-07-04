621818b64ac8fc2c6d4be80fcc5211d1c9f40636
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-07-04 06:49:16 +0100

    second problem

---
+++ codeforces/icpc_mansoura/ct_0/B_ Hmmmm.py
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int main(){
+    
+    int n,a,b,x,s=0;
+    
+    std::cin>>n;
+    
+    // do{
+    std::cin>>a>>b;
+    // }while(a>b);
+    
+    for(int i=0; i<n ; i++){
+        
+    std::cin>>x;
+    
+    if(x>=a and x<=b)
+        s+=x;
+    }
+    
+    std::cout<<s;
+    
+    
+    
+}
