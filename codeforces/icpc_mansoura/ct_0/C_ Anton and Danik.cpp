534e8165b4fa2d3e7c2498c7583aa1c4f4b6f68a
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-07-04 23:14:44 +0100

    Solved ct0_c

---
+++ codeforces/icpc_mansoura/ct_0/C_ Anton and Danik.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std ;
+ 
+int main(){
+    
+    int n ; 
+    int a=0 , d=0 ; 
+    cin >> n;
+    while(n--){
+        char c ;
+        cin >> c ;
+        if(c == 'A')
+            a++;
+        if(c == 'D')
+            d++;
+    }
+    
+    if(a>d)
+        cout << "Anton";
+    else if(a<d)
+        cout << "Danik";
+    else
+        cout << "Friendship";
+    
+}
