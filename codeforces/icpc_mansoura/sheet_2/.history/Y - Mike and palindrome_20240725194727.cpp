48034c24ae3bb67ec21c0a7f9d489189304dc6e8
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-07-25 20:21:11 +0100

    added comments

---
+++ codeforces/icpc_mansoura/sheet_2/.history/Y - Mike and palindrome_20240725194727.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    
+    string s; 
+    cin >> s ;
+    
+    short i,l=s.length() ; 
+    bool move=false,pal=true ; 
+    
+    for(i=0 ; i < l/2 ; i++)
+        if(s[i] != s[l-1-i] ){
+            s[i] = s[l-1-i] ; // changing the character
+
+            move =true;
+            break ; // we performed exaclty one move 
+        
+        }
+
+    //we should discuss it when i==l/2 esp when l is odd
+        
+    if(move){
+        for(i=0 ; i < l/2 ; i++)
+            if(s[i] != s[l-1-i] ){
+                cout << "NO";
+                pal = false;
+                return 0;
+            }
+        if(pal)
+            cout << "YES";
+            
+    }
+    else{
+        // cout << l%2 << endl;
+        if(!(l%2))
+            cout << "NO";
+        else
+            cout << "YES" ; 
+        
+        return 0 ; 
+    }
+        
+    //  "abcba"     =>      YES     because one change can make it pal
+    //  abzba, abyba , abwba ....
+    
+    //  "abccba" => NO 
+    // needs two changes like "abwwba" ... 
+
+
+
+    // for another time they adopted a method that doesnt require all this calculation tragedy !!!  
+    
+    
+}
