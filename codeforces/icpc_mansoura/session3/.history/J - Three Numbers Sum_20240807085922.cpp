e3099375321a74698c307c7f4f93fab6a1fa1b01
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-08-07 09:03:41 +0100

    tried with j and still a small problem to be fixed

---
+++ codeforces/icpc_mansoura/session3/.history/J - Three Numbers Sum_20240807085922.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+#define ll long long
+#define f(n) for(int i=1 ; i<= n ; i++)
+#define ff(n) for(int j=1 ; j<= n ; j++)
+#define fff(n) for(int l=1 ; l<= n ; l++)
+
+int main(){
+        
+    int k , s ;
+    cin >> k >> s ;
+    
+    int ct = 0; 
+    // maximum value 
+    if(s== 3*k)
+        ct = 1 ; 
+        
+    else{
+        f(k)
+            ff(k)
+                 ct++ ;  
+                
+    }
+            
+    cout << ct ; 
+    
+
+
+}
+
---
+++ codeforces/icpc_mansoura/session3/.history/J - Three Numbers Sum_20240807090219.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+#define ll long long
+#define f(n) for(int i=1 ; i<= n ; i++)
+#define ff(n) for(int j=1 ; j<= n ; j++)
+#define fff(n) for(int l=1 ; l<= n ; l++)
+
+int main(){
+        
+    int k , s ;
+    cin >> k >> s ;
+    
+    int ct = 0; 
+    // maximum value 
+    if(s== 3*k)
+        ct = 1 ; 
+        
+    else{
+        f(k)
+            ff(k){
+                int z =s - i - j ;
+                if(z>=0 && z<=k)
+                    ct++ ;
+            }
+                
+    }
+            
+    cout << ct ; 
+    
+
+
+}
+
