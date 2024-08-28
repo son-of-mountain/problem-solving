5cbd4d6bae5a65d72b66596d8a27b681d3895e99
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-08-28 22:02:29 +0100

    nice try but should brute force > Verfiy Pass

---
+++ codeforces/Verify Password.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define sonic ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define y cout<< "YES\n"
+#define no cout<< "NO\n"
+
+int main() {
+    sonic ;
+
+    int t ; cin >> t ;
+    while (t--) {
+        int n ; cin >> n ;
+        string s ; cin >> s ;
+        for(int i = 0 ; i < n-1 ; i++) {
+            for(int j = 1 ; j < n ; j++) {
+                if(s[i] > s[j]) { // 3 and 4 condition
+                    no; break;
+                }
+                else
+                    y;
+
+
+            }
+
+        }
+    }
+
+    return 0 ;
+}
---
+++ codeforces/icpc_mansoura/.history/session4/A- Max Box_20240824143431.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define sonic ios_base::sync_with_stdio(false) ; cin.tie(NULL) ; cout.tie(NULL);
+
+
+int main(){
+    
+    sonic ;
+
+    int pre[100001];
+    int arr[100001];
+    int n ; cin >> n ;
+
+
+
+
+
+
+
+
+
+
+    return 0 ;
+}
