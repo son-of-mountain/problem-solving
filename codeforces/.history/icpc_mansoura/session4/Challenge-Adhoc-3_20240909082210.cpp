3a27c70a0f92b2b4534981e1e34335d790fa9cc0
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-09-09 08:24:31 +0100

    simple solution for the 3rd challenge

---
+++ codeforces/.history/icpc_mansoura/session4/Challenge-Adhoc-3_20240909082210.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define sonic ios_base::sync_with_stdio(false) ; cin.tie(NULL) ; cout.tie(NULL);
+
+// stopped at 1:04:37
+
+
+int main(){
+    
+    sonic ;
+
+     
+    int v,n,q ; cin >> n >> q;
+
+    int l,r; 
+    int arr[n]={};
+    for(int i=1 ; i <= n ; i++)
+        cin >> arr[i] ; 
+
+
+    while(q--){
+        cin >> v >> l >> r; 
+        for(int i=l ; i <= r ; i++){
+            arr[i] += v ;
+        }
+        for(int i=1 ; i<=n ; i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+    }
+
+
+    return 0 ;
+}
