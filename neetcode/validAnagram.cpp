95ce0a336be14527de60c2f2ba17f28d4d05e00d
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2025-01-31 09:58:26 +0100

    first approach: using two frq

--- neetcode/validAnagram.cpp
+++ neetcode/validAnagram.cpp
@@ -1,3 +1,43 @@
-class Solution{
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false ; 
+        
+        int sf[26]={0},tf[26]={0};
+        
+        for(int i = 0 ; i< s.size() ; ++i){
+            int c = s[i] - 'a';
+            sf[ c ]++;
+        }
 
-}
+        for(int i = 0 ; i< s.size() ; ++i){
+            int c = t[i]-'a';
+            tf[ c ]++;
+        }
+        for(int i = 0 ; i < 26 ; ++i){
+            if(sf[i] != tf[i])
+                return false;
+        }
+        return true; 
+
+    }
+};
+
+    /*
+    store both strings on an unordered_set 
+    store their size as well for edge cases  
+
+    if(s.size() != t.size())
+        return false;
+
+
+    unordered_set<char> ss(s.begin(),s.end()),tt(t.begin(),t.end());
+    if(ss.size() != tt.size())
+        return false;
+
+    for(char sss: s){
+        if(!tt.count(sss))
+            return false; 
+    }
+    return true ; */
