5b29825c792352d8379cff135a272335bda527f8
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2025-02-14 11:32:04 +0100

    6th/cses is understood and fully solved

--- game-of-codes/Apartments.cpp
+++ game-of-codes/Apartments.cpp
@@ -8,29 +8,32 @@ int main(){
 
 
 	int a[n];
-	for(int i = 0 ; i < n ; ++i)
+	for(int i = 0 ; i < n ; ++i) 
 		cin >> a[i];
 
 	int b[m];
-	for(int i= 0 ; i < m ; ++i)
+	for(int i= 0 ; i < m ; ++i) 
 		cin >> b[i];
 
 	sort(a,a+n);
 	sort(b,b+m);
 
-	int pos = 0 , app=0 ;
+	int match=0,i=0,j=0; 
 
-	for(int i = 0 ; i < n ; ++i)
-	{	pos = 0;
-		for(int j=pos ; j < m ; ++j){
-			
-			if( a[i] <= b[j]+k && a[i] >= b[j]-k){
-				app++;
-				pos = j+1;}
-		}
+	while( i < n){
+	
+		while( j < m && b[j] < a[i] - k) j++;
+
+		if( abs( a[i] - b[j] ) <=k && j<m){
+			match++;
+			i++;
+			j++;}
+
+		else
+			i++;
 	}
-		
 
-	cout << app;
+	cout << match ; 
+
 
 }
